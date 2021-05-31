79089010950702ef91278958e8290bc36af3f3e5
Lee Turner <lee@leeturner.me>
2021-05-31 22:26:15 +0100

    LT - initial commit of service scaffolding

---
+++ src/main/kotlin/com/leeturner/dailyroverapi/DailyRoverApiApplication.kt
@@ -0,0 +1,11 @@
+package com.leeturner.dailyroverapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DailyRoverApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DailyRoverApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/leeturner/dailyroverapi/DailyRoverApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.leeturner.dailyroverapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DailyRoverApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
