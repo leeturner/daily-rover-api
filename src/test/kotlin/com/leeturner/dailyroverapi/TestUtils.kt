7a763208097099b738c0b3a036bb24db67cc1351
Lee Turner <lee@leeturner.me>
2021-08-07 10:51:15 +0100

    LT - Update the test utils class to return a NasaPhotoResponse.kt

--- src/test/kotlin/com/leeturner/dailyroverapi/TestUtils.kt
+++ src/test/kotlin/com/leeturner/dailyroverapi/TestUtils.kt
@@ -2,14 +2,14 @@ package com.leeturner.dailyroverapi
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import com.leeturner.dailyroverapi.nasa.model.NasaResponse
+import com.leeturner.dailyroverapi.nasa.model.photo.NasaPhotoResponse
 
 object TestUtils {
 
     private val objectMapper: ObjectMapper = ObjectMapper().registerModule(KotlinModule())
 
-    fun getNasaResponse(fileName: String): NasaResponse {
+    fun getNasaResponse(fileName: String): NasaPhotoResponse {
         val inputStream = this.javaClass.getResourceAsStream("/json/nasa-response/$fileName")
-        return objectMapper.readValue(inputStream, NasaResponse::class.java)
+        return objectMapper.readValue(inputStream, NasaPhotoResponse::class.java)
     }
 }
