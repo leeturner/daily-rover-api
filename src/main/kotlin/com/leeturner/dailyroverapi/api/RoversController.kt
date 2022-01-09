9b69c60d168c4f7612c41683973b1bc75a77e7e5
Lee Turner <lee@leeturner.me>
2022-01-09 18:44:29 +0000

    LT - rename the rovers controller to be consistent with the other controller

--- src/main/kotlin/com/leeturner/dailyroverapi/api/RoversController.kt
+++ src/main/kotlin/com/leeturner/dailyroverapi/api/RoversController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("v1/rovers")
-class RoversApiController(
+class RoversController(
     private val marsRovers: MarsRovers
 ) {
     @GetMapping("/")
