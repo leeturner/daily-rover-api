47c3b08d23fdb5c572c320966ea14cad9dcc2172
Lee Turner <lee@leeturner.me>
2022-02-06 14:19:32 +0000

    LT - add default endpoint to return photos for yesterday and add error handling for future dates

--- src/main/kotlin/com/leeturner/dailyroverapi/api/MarsPhotosController.kt
+++ src/main/kotlin/com/leeturner/dailyroverapi/api/MarsPhotosController.kt
@@ -1,26 +1,37 @@
 package com.leeturner.dailyroverapi.api
 
+import com.leeturner.dailyroverapi.exception.FutureDateException
 import com.leeturner.dailyroverapi.nasa.model.photo.NasaPhotoResponse
 import com.leeturner.dailyroverapi.service.MarsPhotosService
 import mu.KotlinLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
 
 private val logger = KotlinLogging.logger {}
 
-@RestController("v1/photos")
+@RestController
+@RequestMapping("v1/photos")
 class MarsPhotosController(
     val marsPhotosService: MarsPhotosService
 ) {
 
+    @GetMapping("/")
+    fun getPhotosForYesterday(): ResponseEntity<NasaPhotoResponse> {
+        logger.info { "Getting photos for yesterday" }
+        return this.getPhotosByEarthDate(LocalDate.now().minusDays(1))
+    }
+
     @GetMapping("/{earthDate}")
     fun getPhotosByEarthDate(@PathVariable earthDate: LocalDate): ResponseEntity<NasaPhotoResponse> {
-        // TODO: return a bad request if earthDate is in the future
+        // we can't return photos for future dates
+        if (earthDate.isAfter(LocalDate.now())) {
+            throw FutureDateException("The earth date can not be a future date")
+        }
         logger.info { "Earth date passed = $earthDate" }
-
         return ResponseEntity.ok(this.marsPhotosService.getMarsPhotosByEarthDate(earthDate))
     }
 }
